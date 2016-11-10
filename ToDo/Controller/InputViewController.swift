77c15ee26396e01b5e6d91ded0d94671eb5b94f9
nsakaimbo <nsakaimbo@users.noreply.github.com>
2016-11-10 10:44:10 -0500

    [InputViewController] Implement in Storyboard

---
+++ ToDo/Controller/InputViewController.swift
@@ -0,0 +1,94 @@
+//
+//  InputViewController.swift
+//  ToDo
+//
+//  Created by Nicholas Sakaimbo on 11/10/16.
+//  Copyright © 2016 nick_skmbo. All rights reserved.
+//
+
+import CoreLocation
+import UIKit
+
+fileprivate extension Selector {
+    static let save = #selector(InputViewController.save)
+}
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    lazy var geocoder = CLGeocoder()
+    
+    var itemManager: ItemManager?
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        return formatter
+    }()
+
+    
+    // Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        saveButton.addTarget(self, action: .save, for: .touchUpInside)
+    }
+    
+    // Methods
+    func save() {
+        
+        guard let title = titleTextField.text,
+            !title.isEmpty else { return }
+        
+        let date: Date?
+        if let dateText = dateTextField.text,
+            !dateText.isEmpty {
+            date = dateFormatter.date(from: dateText)
+        } else {
+            date = nil
+        }
+        
+        let description: String?
+        if let descriptionText = descriptionTextField.text,
+            !descriptionText.isEmpty {
+            description = descriptionText
+        } else {
+            description = nil
+        }
+        
+        if let locationName = locationTextField.text,
+            !locationName.isEmpty {
+            
+            // Save Location with title and coordinate
+            if let address = addressTextField.text,
+                !address.isEmpty {
+                
+                geocoder.geocodeAddressString(address) { (placeMarks, error) in
+                    
+                    let placeMark = placeMarks?.first
+                    
+                    let item = ToDoItem(title: title, itemDescription: description, timestamp: date?.timeIntervalSince1970, location: Location(name: locationName, coordinate: placeMark?.location?.coordinate))
+                    
+                    self.itemManager?.addItem(item)
+                }
+            } else {
+                // Save Location with title only
+                let item = ToDoItem(title: title, itemDescription: description, timestamp: date?.timeIntervalSince1970, location: Location(name: locationName, coordinate: nil))
+                
+                self.itemManager?.addItem(item)
+            }
+        } else {
+            // Save ToDoItem with no Location
+            let item = ToDoItem(title: title, itemDescription: description, timestamp: date?.timeIntervalSince1970, location: nil)
+            
+            self.itemManager?.addItem(item)
+        }
+    }
+}
---
+++ ToDo/Controller/InputViewControllerTests.swift
@@ -0,0 +1,152 @@
+//
+//  InputViewControllerTests.swift
+//  ToDo
+//
+//  Created by Nicholas Sakaimbo on 11/10/16.
+//  Copyright © 2016 nick_skmbo. All rights reserved.
+//
+
+import XCTest
+@testable
+import ToDo
+import CoreLocation
+
+class InputViewControllerTests: XCTestCase {
+    
+    var sut: InputViewController!
+    var placemark: MockPlacemark!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: InputViewController.self)) as! InputViewController
+        
+        _ = sut.view
+    }
+    
+    func test_HasTitleTextField() {
+        XCTAssertNotNil(sut.titleTextField)
+    }
+    
+    func test_HasDateTextField() {
+        XCTAssertNotNil(sut.dateTextField)
+    }
+    
+    func test_HasLocationTextField() {
+        XCTAssertNotNil(sut.locationTextField)
+    }
+    
+    func test_HasAddressTestField() {
+        XCTAssertNotNil(sut.addressTextField)
+    }
+    
+    func test_HasDescriptionTextField() {
+        XCTAssertNotNil(sut.descriptionTextField)
+    }
+    
+    func test_HasSaveButton() {
+        XCTAssertNotNil(sut.saveButton)
+    }
+    
+    func test_HasCancelButton() {
+        XCTAssertNotNil(sut.cancelButton)
+    }
+    
+    func testSave_UsesGeocoderToGetCoordinateFromAddress() {
+        sut.titleTextField.text = "test title"
+        sut.dateTextField.text = "02/22/2016"
+        sut.locationTextField.text = "test location"
+        sut.addressTextField.text = "infinite loop"
+        sut.descriptionTextField.text = "test description"
+        
+        let mockGeocoder = MockGeocoder()
+        sut.geocoder = mockGeocoder
+        
+        sut.itemManager = ItemManager()
+        
+        sut.save()
+        
+        placemark = MockPlacemark()
+        let coordinate = CLLocationCoordinate2D(latitude: 37.3316851, longitude: -122.0300674)
+        placemark.mockCoordinate = coordinate
+        mockGeocoder.completionHandler?([placemark], nil)
+        
+        let item = sut.itemManager?.itemAtIndex(0)
+        
+        let testItem = ToDoItem(title: "test title", itemDescription: "test description", timestamp: 1456117200, location: Location(name: "test location", coordinate: coordinate))
+        
+        XCTAssertEqual(item, testItem)
+    }
+    
+    func testSave_CreatesToDoItemWithTitleDateAndDescription() {
+        sut.titleTextField.text = "test title"
+        sut.descriptionTextField.text = "test description"
+        sut.dateTextField.text = "02/22/2016"
+        
+        sut.itemManager = ItemManager()
+        
+        sut.save()
+        
+        let item = sut.itemManager?.itemAtIndex(0)
+        
+        let testItem = ToDoItem(title: "test title",
+                                itemDescription: "test description",
+                                timestamp: 1456117200)
+        
+        XCTAssertEqual(item, testItem)
+    }
+    
+    func testSave_SuccessWithTitleOnly() {
+        sut.titleTextField.text = "test title"
+        
+        sut.itemManager = ItemManager()
+        
+        sut.save()
+        
+        let item = sut.itemManager?.itemAtIndex(0)
+        
+        let testItem = ToDoItem(title: "test title")
+        
+        XCTAssertEqual(item, testItem)
+    }
+    
+    func testSaveButton_HasSaveAction() {
+        let saveButton = sut.saveButton
+        
+        guard let action = saveButton?.actions(forTarget: sut, forControlEvent: .touchUpInside)?.first else {
+            XCTFail()
+            return
+        }
+     
+        XCTAssertEqual(action, "save")
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+}
+
+extension InputViewControllerTests {
+    
+    class MockGeocoder: CLGeocoder {
+        
+        var completionHandler: CLGeocodeCompletionHandler?
+        
+        override func geocodeAddressString(_ addressString: String, completionHandler: @escaping CLGeocodeCompletionHandler) {
+            self.completionHandler = completionHandler
+        }
+    }
+    
+    class MockPlacemark: CLPlacemark {
+        
+        var mockCoordinate: CLLocationCoordinate2D?
+        
+        override var location: CLLocation? {
+            guard let coordinate = mockCoordinate else { return CLLocation() }
+            
+            return CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        }
+    }
+}
