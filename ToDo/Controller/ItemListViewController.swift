261ce0f889878281a56e489009602b9828583e3f
nsakaimbo <nsakaimbo@users.noreply.github.com>
2016-11-26 12:30:01 -0500

    [Storyboard] Set up initial view controller

--- ToDo/Controller/ItemListViewController.swift
+++ ToDo/Controller/ItemListViewController.swift
@@ -13,8 +13,20 @@ class ItemListViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     @IBOutlet var tableViewDelegateDataSource: (UITableViewDataSource & UITableViewDelegate)!
     
+    let itemManager = ItemManager()
+    
     override func viewDidLoad() {
         tableView.dataSource = tableViewDelegateDataSource
         tableView.delegate = tableViewDelegateDataSource
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let nextViewController = storyboard.instantiateViewController(withIdentifier: String(describing: InputViewController.self)) as? InputViewController {
+            
+            nextViewController.itemManager = itemManager
+            present(nextViewController, animated: true, completion: nil)
+        }
     }
 }
--- ToDo/Storyboards/StoryboardTests.swift
+++ ToDo/Storyboards/StoryboardTests.swift
@@ -12,24 +12,60 @@ import ToDo
 
 class StoryboardTests: XCTestCase {
     
+    var sut: ItemListViewController!
+    var addButton: UIBarButtonItem!
+    
     override func setUp() {
         super.setUp()
-       
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        
+        guard let rootViewController = navigationController.viewControllers.first as? ItemListViewController else {
+            assertionFailure("Error. Initial view controller is not ItemListViewController.")
+            return
+        }
+        
+        sut = rootViewController
+        
+        guard let button = sut.navigationItem.rightBarButtonItem else {
+            assertionFailure("Error. Could not establish reference to add UIBarButtonItem.")
+            return
+        }
+        
+        addButton = button
+        
+        // NOTE: - Contrary to the example (which was written in Swift 2.2), we need to assign the navigation controller, and NOT the view controller, to the shared keyWindow property
+        UIApplication.shared.keyWindow?.rootViewController = navigationController
+        
+        assert(sut.presentedViewController == nil)
+        
+         sut.perform(#selector(ItemListViewController.addButtonPressed(_:)), with: addButton)
+    }
+    
+    func testNavigationController_RightBarButtonItem_IsAddWithSelfAsTarget() {
+        XCTAssertEqual(sut.navigationItem.rightBarButtonItem?.target as? UIViewController, sut)
+    }
+    
+    func testAddItem_PresentsAddItemViewController() {
+        XCTAssertNotNil(sut.presentedViewController)
+        XCTAssertTrue(sut.presentedViewController is InputViewController)
+    }
+    
+    func testItemListVC_SharesItemManagerWithInputVC() {
+        
+        let inputViewController = sut.presentedViewController as! InputViewController
+        
+        guard let inputItemManager = inputViewController.itemManager else {
+            XCTFail(); return;
+        }
+        
+        XCTAssertTrue(sut.itemManager === inputItemManager)
     }
     
     override func tearDown() {
-        
+        sut = nil
         super.tearDown()
     }
-    
-    func testExample() {
-       
-    }
-    
-    func testPerformanceExample() {
-        self.measure {
-            
-        }
-    }
-    
 }
