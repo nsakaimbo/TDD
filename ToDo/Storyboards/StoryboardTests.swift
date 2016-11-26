d2907713d44d0caa10fc5093a87167de821a26ba
nsakaimbo <nsakaimbo@users.noreply.github.com>
2016-11-26 12:16:51 -0500

    [Storyboard] Add StoryboardTests file

---
+++ ToDo/Storyboards/StoryboardTests.swift
@@ -0,0 +1,35 @@
+//
+//  StoryboardTests.swift
+//  ToDo
+//
+//  Created by Nicholas Sakaimbo on 11/26/16.
+//  Copyright © 2016 nick_skmbo. All rights reserved.
+//
+
+import XCTest
+@testable
+import ToDo
+
+class StoryboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+       
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+    
+    func testExample() {
+       
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+            
+        }
+    }
+    
+}
